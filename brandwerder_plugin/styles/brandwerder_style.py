146df04e6e035af6269285bd9c8f9dd3b077863f
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2017-10-10 11:12:33 +0200

    Use brandwerder-style name

--- brandwerder_plugin/styles/brandwerder_style.py
+++ brandwerder_plugin/styles/brandwerder_style.py
@@ -2,7 +2,7 @@ from zope.interface import implementer
 from mailman.interfaces.styles import IStyle
 @implementer(IStyle)
 class BrandwerderStyle:
-    name = 'a-test-style'
+    name = 'brandwerder-style'
     def apply(self, mailing_list):
         # Just does something very simple.
         mailing_list.display_name = 'TEST STYLE LIST'
