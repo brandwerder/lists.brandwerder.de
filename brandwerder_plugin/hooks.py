fb7c870dd6508d4acc612c057ea9cdaa34511d86
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2017-10-15 00:47:14 +0200

    fixup

--- brandwerder_plugin/hooks.py
+++ brandwerder_plugin/hooks.py
@@ -3,17 +3,17 @@ import os
 # from mailman.interfaces.plugin import IPlugin
 from public import public
 from zope.interface import implementer
-from .styles.brandwerder_style import BrandwerderStyle
-from .templates.brandwerder_template import BrandwerderTemplate
+# from .styles.brandwerder_style import BrandwerderStyle
+# from .templates.brandwerder_template import BrandwerderTemplate
 
 @public
 # @implementer(IPlugin)
 class BrandwerderPlugin:
     def pre_hook(self):
-        manager.register(BrandwerderStyle())
+        # manager.register(BrandwerderStyle())
 
     def post_hook(self):
-        BrandwerderTemplate.apply()
+        # BrandwerderTemplate.apply()
 
     @property
     def resource(self):
