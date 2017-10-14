02372b84fe10a13e405a6990a6ff3d8230ff37d0
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2017-10-15 00:45:32 +0200

    FIXUP

--- brandwerder_plugin/styles/__init__.py
+++ brandwerder_plugin/styles/__init__.py
@@ -5,9 +5,9 @@ from zope.component import getUtility
 from mailman.interfaces.styles import IStyleManager
 manager = getUtility(IStyleManager)
 
-#for style in manager.styles:
-#    print(style.name)
-#print()
+for style in manager.styles:
+   print(style.name)
+print()
 
 # prevent double inclusion
 if manager.get('brandwerder-style') is None:
