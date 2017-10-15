ab2ffe8b776f13b53e34f39c4a3066443e215128
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2017-10-15 10:05:15 +0200

    FIXUP setup templates

--- brandwerder_plugin/templates/brandwerder_template.py
+++ brandwerder_plugin/templates/brandwerder_template.py
@@ -21,7 +21,7 @@ class BrandwerderTemplate:
             print(name + ": " + file_path)
             print(name + ": " + str(file_abs_path))
             print(name + ": " + file_uri)
-            print(name + " exists?: " + file_abs_path.exists())
+            print(name + " exists?: " + str(file_abs_path.exists()))
             # manager.set(name, None, file_uri)
             template = manager.get(name, None)
             print(template)
