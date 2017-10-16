4f46f09b864e43734f53d4adf8e84e5a5637c682
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2017-10-16 10:33:02 +0200

    set template in style

--- brandwerder_plugin/styles/brandwerder_style.py
+++ brandwerder_plugin/styles/brandwerder_style.py
@@ -4,6 +4,7 @@ from mailman.core.i18n import _
 from mailman.interfaces.archiver import ArchivePolicy
 from mailman.interfaces.styles import IStyle, IStyleManager
 from mailman.interfaces.mailinglist import SubscriptionPolicy
+from ..templates.brandwerder_template import BrandwerderTemplate
 from public import public
 import re
 
@@ -36,6 +37,9 @@ class BrandwerderStyle:
         mlist.display_name = re.sub(r'klasse-(.*)', BrandwerderStyle.klassenlist_name, mlist.list_name)
         mlist.preferred_language = 'de'
         mlist.subject_prefix = _('[$mlist.display_name] ')
+
+        if re.match('klasse', mlist.list_name):
+            BrandwerderTemplate.set_template('list:user:notice:welcome', mlist.list_name, 'list:user:notice:welcome-klasse.txt')
 
         # Description
         mlist.description = _('Die Mailingliste der $mlist.display_name')
--- brandwerder_plugin/templates/brandwerder_template.py
+++ brandwerder_plugin/templates/brandwerder_template.py
@@ -9,22 +9,28 @@ class BrandwerderTemplate:
     name = 'brandwerder-template'
 
     @staticmethod
-    def apply():
+    def set_template(name, context, uri):
         current_dir = os.path.dirname(os.path.abspath(__file__)) + '/de/'
         manager = getUtility(ITemplateManager)
+
+        file_path = current_dir + uri
+        file_abs_path = pathlib.Path(file_path).resolve()
+        # file_uri = file_abs_path.as_uri()
+        file_uri = 'file://' + str(file_abs_path)
+        # print(name + ": " + file_path)
+        # print(name + ": " + str(file_abs_path))
+        # print(name + ": " + file_uri)
+        # print(name + " exists?: " + str(file_abs_path.exists()))
+        if file_abs_path.exists():
+            manager.set(name, None, file_uri)
+            # template = manager.get(name, None)
+            # print(template)
+        # print()
+
+    @staticmethod
+    def apply():
         for name, uri in ALL_TEMPLATES.items():
             if uri is None:
                 continue
-            file_path = current_dir + uri
-            file_abs_path = pathlib.Path(file_path).resolve()
-            # file_uri = file_abs_path.as_uri()
-            file_uri = 'file://' + str(file_abs_path)
-            # print(name + ": " + file_path)
-            # print(name + ": " + str(file_abs_path))
-            # print(name + ": " + file_uri)
-            # print(name + " exists?: " + str(file_abs_path.exists()))
-            if file_abs_path.exists():
-                manager.set(name, None, file_uri)
-                # template = manager.get(name, None)
-                # print(template)
-            # print()
+
+            BrandwerderTemplate.set_template(name, None, uri)
